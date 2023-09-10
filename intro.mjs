96300dffa98f6db30d22ba40d862b125edb0eb96
MdKaioumislam5 <afrankaiyum34@gmail.com>
2023-09-10 18:59:34 +0600

    updated puppeteer

--- intro.mjs
+++ intro.mjs
@@ -1,17 +1,52 @@
+// import puppeteer from "puppeteer";
+// import { setTimeout } from "timers/promises";
+
+// const browser = await puppeteer.launch({
+//   headless: false,
+//   defaultViewport: { width: 1920, height: 1080 },
+//   slowMo: 250,
+//   userDataDir: "temporary",
+// });
+
+// const page = await browser.newPage();
+
+// await page.goto("https://duckduckgo.com/", { waitUntil: "networkidle2" });
+// await page.waitForSelector("#search_form_input_homepage");
+// await page.type("#search_form_input_homepage", "devconfbd");
+// await page.click("#search_button_homepage");
+// await page.waitForSelector('[data-testid="result-title-a"]');
+// await page.screenshot({ path: "duckduckgo" });
+// await browser.close();
+
 import puppeteer from "puppeteer";
 
 const browser = await puppeteer.launch({
   headless: false,
   defaultViewport: { width: 1920, height: 1080 },
-  slowMode: 250,
   userDataDir: "temporary",
 });
 
 const page = await browser.newPage();
-await page.goto("https://www.w3schools.com/", {
-  waitUntil: "networkidle2",
-  timeout: 60000,
-});
 
-await page.screenshot({ path: "w3schools.png" });
+await page.goto("https://duckduckgo.com/", { waitUntil: "networkidle2" });
+await page.waitForSelector([(id *= "searchbox_input")]);
+await page.type([(id *= "searchbox_input")], "devconfbd");
+// await page.click([(id *= "searchbox_input")]);
+// await page.waitForSelector('[data-testid="result-title-a"]');
+await page.goto("https://devconfbd.com/", { waitUntil: "networkidle2" });
+
+await page.screenshot({ path: "duckduckgo.png" });
 await browser.close();
+
+// await page.goto("https://devconfbd.com/");
+// await page.screenshot({ path: "devconfbd.png" });
+
+// const guestElement = await page.waitForSelector("img[alt='guest']");
+// await guestElement.scrollIntoView();
+// await setTimeout(1000);
+
+// await guestElement.click("img[alt='guest']");
+// await setTimeout(1000);
+
+// await page.screenshot({ path: "guest.png" });
+// await browser.close();
---
+++ temporary/optimization_guide_model_store/25/E6DC4029A1E4B4C1/6ADD59A65698CE86/model-info.pb
@@ -0,0 +1,4 @@
+Ç»Ÿd 
+2e
+_type.googleapis.com/google.internal.chrome.optimizationguide.v1.ClientSidePhishingModelMetadata:­
+ª/media/kaiyum/Document/web-automation/explore-puppeteer/temporary/optimization_guide_model_store/25/E6DC4029A1E4B4C1/6ADD59A65698CE86/visual_model_20230727_desktop.tfliteB€´¼
