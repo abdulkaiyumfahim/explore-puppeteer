41a4cffa17f4f4322aca7409b40c386c2816d7ac
MdKaioumislam5 <afrankaiyum34@gmail.com>
2023-09-16 20:13:00 +0600

    explore-puppeteer complete

--- intro.mjs
+++ intro.mjs
@@ -13,6 +13,7 @@ await page.goto("https://duckduckgo.com/", { waitUntil: "networkidle2" });
 await page.waitForSelector("#searchbox_input");
 await page.type("#searchbox_input", "devconfbd");
 await page.click(`[aria-label="Search"]`);
+// await page.waitForSelector(`[aria-label="Search Results"]`);
 try {
   await page.waitForSelector(`[aria-label="Search Results"]`, {
     timeout: 60000,
