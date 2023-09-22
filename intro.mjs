bb75b835e3fde9719be9502b31058477df0bc527
MdKaioumislam5 <afrankaiyum34@gmail.com>
2023-09-22 23:12:33 +0600

    updated-puppeteer-explore

--- intro.mjs
+++ intro.mjs
@@ -7,6 +7,13 @@ const browser = await puppeteer.launch({
   userDataDir: "temporary",
 });
 
+// const browser = await puppeteer.launch({
+//   headless: false,
+//   defaultViewport: { width: 1920, height: 1080 },
+//   slowMo: 250,
+//   userDataDir: "temporary",
+// });
+
 const page = await browser.newPage();
 
 await page.goto("https://duckduckgo.com/", { waitUntil: "networkidle2" });
